4bc5789a798b450fb60ac838647a7f52b6ddcd06
HumoFX <humo_otahanov@icloud.com>
2022-02-19 22:33:33 +0500

    constants changed

--- bot/base_commands.py
+++ bot/base_commands.py
@@ -6,6 +6,13 @@ from bot.models import Command
 logger = logging.getLogger(__name__)
 
 
+# create function to check if text is phone number with plus and digits only and return True or False
+def is_phone_number(text):
+    if text.startswith('+') and text[1:].isdigit():
+        return True
+    return False
+
+
 def register(bot, user, update):
     if update.message.contact:
         user.phone_number = update.message.contact.phone_number
@@ -18,7 +25,17 @@ def register(bot, user, update):
                                       message_id=update.message.message_id,
                                       text=constants.register,
                                       to_menu=constants.home)
-
+    elif update.message.text and is_phone_number(update.message.text):
+        user.phone_number = update.message.text
+        user.is_registered = True
+        user.save()
+        bot.sendMessage(chat_id=user.telegram_id,
+                        text=constants.register_succeed,
+                        reply_markup=markups.home_markup('ru'))
+        return Command.objects.create(user=user,
+                                      message_id=update.message.message_id,
+                                      text=constants.register,
+                                      to_menu=constants.home)
     elif user.phone_number is None:
         bot.sendMessage(update.message.chat_id,
                         text=constants.ask_contact,
